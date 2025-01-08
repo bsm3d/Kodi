7301d61ae7c2660fb46c244fc5e7a1479d8842bb
Benoît Saint-Moulin <benoit.saint-moulin@heaj.be>
2025-01-08 21:59:55 +0100

    Add files via upload

---
+++ Internet Archive/Sources/plugin.video.bsm3d.archive.org/resources/default.py
@@ -0,0 +1,26 @@
+"""
+    Addon : KODI - Internet Archive Browser
+    Version 1.0.0 - Kodi 21 (Omega) or Upper
+    Author: BSM3D
+    Description: This Kodi addon allows users to browse and search the Internet Archive's video and audio collections
+    Copyright (C) 2025 BSM3D
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import sys
+from resources.lib.main import Main
+
+if __name__ == '__main__':
+    Main()
